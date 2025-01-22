f35b5427ceb4bd98e99d1b0a14a168f7e55b3bca
T036875 <mariana.viana.contractor@bbva.com>
2025-01-22 11:04:07 +0100

    fix: routes of test

--- src/pods/employee/employee.mappers.ts
+++ src/pods/employee/employee.mappers.ts
@@ -1,4 +1,4 @@
-import { mapToCollection } from 'common/mappers';
+import { mapToCollection } from '../../common/mappers/index';
 import * as apiModel from './api/employee.api-model';
 import * as viewModel from './employee.vm';
 
--- src/pods/project/project.mapper.ts
+++ src/pods/project/project.mapper.ts
@@ -1,4 +1,4 @@
-import { mapToCollection } from 'common/mappers';
+import { mapToCollection } from '../../common/mappers/index';
 import * as apiModel from './api/project.api-model';
 import * as viewModel from './project.vm';
 
